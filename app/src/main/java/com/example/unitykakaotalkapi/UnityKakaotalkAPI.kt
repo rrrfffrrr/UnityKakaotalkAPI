f98f776b30a7a9779e87a197ba3d424827f01a9f
KCY <rrrfffrrr@naver.com>
2021-01-18 19:34:41 +0900

    Add login code

--- app/src/main/java/com/example/unitykakaotalkapi/UnityKakaotalkAPI.kt
+++ app/src/main/java/com/example/unitykakaotalkapi/UnityKakaotalkAPI.kt
@@ -3,6 +3,7 @@ package com.example.unitykakaotalkapi
 import android.content.Context
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.auth.AuthCodeHandlerActivity;
+import com.kakao.sdk.auth.LoginClient
 import com.unity3d.player.UnityPlayer;
 
 class UnityKakaotalkAPI {
@@ -10,12 +11,24 @@ class UnityKakaotalkAPI {
         private lateinit var context: Context;
         fun Initialize(context: Context, key: String) {
             this.context = context;
-
-
-
             KakaoSdk.init(context, key);
         }
 
-
+        fun TryLogin(gameObject: String) {
+            if (LoginClient.instance.isKakaoTalkLoginAvailable(context)) {
+                LoginClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    if (error != null) {
+                        // login fail
+                        UnityPlayer.UnitySendMessage(gameObject, "OnLoginFail", error.message);
+                    } else if (token != null) {
+                        // login success token.accessToken
+                        UnityPlayer.UnitySendMessage(gameObject, "OnLoginSuccess", token.accessToken);
+                    }
+                }
+            } else {
+                // login fail cause no kakaotalk
+                UnityPlayer.UnitySendMessage(gameObject, "OnLoginFail", "No kakaotalk installed");
+            }
+        }
     }
 }
