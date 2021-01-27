834228146ec4f151491667adc33dad088c14d6a1
Chan-yeong Kang <rrrfffrrr@naver.com>
2021-01-27 17:47:23 +0900

    Refactoring some codes

--- Unity/UnityKakaotalkAPI/Scripts/KakaoSdk.cs
+++ Unity/UnityKakaotalkAPI/Scripts/KakaoSdk.cs
@@ -5,6 +5,9 @@ namespace Kakaotalk
 {
     public static class KakaoSdk
     {
+        public const string RECEIVER_OBJECT_NAME = "Kakaotalk plugin result receiver";
+        public const string TAG = "kakaotalk plugin";
+
         [System.Flags]
         public enum LOGIN_METHOD {
             Error = 0,
@@ -12,17 +15,21 @@ namespace Kakaotalk
             KakaoAccount = 2,
             Both = 3,
         }
-        public const string RECEIVER_OBJECT_NAME = "Kakaotalk plugin result receiver";
-        public const string TAG = "kakaotalk plugin";
-
+        public static class Order {
+            public const string ASC = "asc";
+            public const string DESC = "desc";
+        }
+        public static class Type {
+            public const string INITIALIZE = "initialize";
+            public const string LOGIN = "login";
+            public const string LOGOUT = "logout";
+            public const string USERINFO = "user information";
+            public const string PROFILE = "profile";
+            public const string FRIENDS = "friends";
+        }
         public const string RESULT_SUCCESS = "success";
         public const string RESULT_FAIL = "fail";
 
-        public const string RESULT_TYPE_INITIALIZE = "initialize";
-        public const string RESULT_TYPE_LOGIN = "login";
-        public const string RESULT_TYPE_LOGOUT = "logout";
-        public const string RESULT_TYPE_USERINFO = "user information";
-        public const string RESULT_TYPE_PROFILE = "profile";
 
         public const string FAIL_RESULT_NOT_AN_ANDROID_DEVICE = "Not an android device";
         public static ResultEvent OnResult = new ResultEvent();
@@ -56,7 +63,7 @@ namespace Kakaotalk
                 }
             }
 #else
-            OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_INITIALIZE, RESULT_FAIL ,FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
+            OnResult?.Invoke(new string[] { TAG, Type.INITIALIZE, RESULT_FAIL ,FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
 #endif
         }
 
@@ -77,28 +84,35 @@ namespace Kakaotalk
                     throw new System.Exception();
         }
 #else
-            OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_LOGIN, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
+            OnResult?.Invoke(new string[] { TAG, Type.LOGIN, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
 #endif
         }
         public static void Logout() {
 #if UNITY_ANDROID && !UNITY_EDITOR
             KakaoSdkObject.Call("Logout");
 #else
-            OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_LOGOUT, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
+            OnResult?.Invoke(new string[] { TAG, Type.LOGOUT, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
 #endif
         }
         public static void GetUserInformation() {
 #if UNITY_ANDROID && !UNITY_EDITOR
             KakaoSdkObject.Call("GetUserInformation");
 #else
-            OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_USERINFO, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
+            OnResult?.Invoke(new string[] { TAG, Type.USERINFO, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
 #endif
         }
         public static void GetProfile() {
 #if UNITY_ANDROID && !UNITY_EDITOR
             KakaoSdkObject.Call("GetProfile");
 #else
-            OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_PROFILE, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
+            OnResult?.Invoke(new string[] { TAG, Type.PROFILE, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
+#endif
+        }
+        public static void GetFriends(int offset, int count, string order) {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            KakaoSdkObject.Call("GetFriends", offset, count, order);
+#else
+            OnResult?.Invoke(new string[] { TAG, Type.FRIENDS, RESULT_FAIL, FAIL_RESULT_NOT_AN_ANDROID_DEVICE });
 #endif
         }
 
@@ -116,28 +130,34 @@ namespace Kakaotalk
         private sealed class KakaoPluginReceiver: MonoBehaviour
         {
             public void OnInitializeSuccess(string reason) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_INITIALIZE, RESULT_SUCCESS, reason});
+                OnResult?.Invoke(new string[] { TAG, Type.INITIALIZE, RESULT_SUCCESS, reason});
             }
             public void OnInitializeFail(string reason) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_INITIALIZE, RESULT_FAIL, reason });
+                OnResult?.Invoke(new string[] { TAG, Type.INITIALIZE, RESULT_FAIL, reason });
             }
             public void OnLoginSuccess(string token) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_LOGIN, RESULT_SUCCESS, token });
+                OnResult?.Invoke(new string[] { TAG, Type.LOGIN, RESULT_SUCCESS, token });
             }
             public void OnLoginFail(string reason) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_LOGIN, RESULT_FAIL, reason });
+                OnResult?.Invoke(new string[] { TAG, Type.LOGIN, RESULT_FAIL, reason });
             }
             public void OnProfileSuccess(string json) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_PROFILE, RESULT_SUCCESS, json });
+                OnResult?.Invoke(new string[] { TAG, Type.PROFILE, RESULT_SUCCESS, json });
             }
             public void OnProfileFail(string reason) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_PROFILE, RESULT_FAIL, reason });
+                OnResult?.Invoke(new string[] { TAG, Type.PROFILE, RESULT_FAIL, reason });
             }
             public void OnUserInformationSuccess(string json) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_USERINFO, RESULT_SUCCESS, json });
+                OnResult?.Invoke(new string[] { TAG, Type.USERINFO, RESULT_SUCCESS, json });
             }
             public void OnUserInformationFail(string reason) {
-                OnResult?.Invoke(new string[] { TAG, RESULT_TYPE_USERINFO, RESULT_FAIL, reason });
+                OnResult?.Invoke(new string[] { TAG, Type.USERINFO, RESULT_FAIL, reason });
+            }
+            public void OnFriendsSuccess(string json) {
+                OnResult?.Invoke(new string[] { TAG, Type.FRIENDS, RESULT_SUCCESS, json });
+            }
+            public void OnFriendsFail(string reason) {
+                OnResult?.Invoke(new string[] { TAG, Type.FRIENDS, RESULT_FAIL, reason });
             }
         }
     }
