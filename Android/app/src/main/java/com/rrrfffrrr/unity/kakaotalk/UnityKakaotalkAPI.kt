b56d5f9bab2fa8708e450aa63ad96705ed644460
KCY <rrrfffrrr@naver.com>
2021-01-28 19:02:20 +0900

    Move callbacks to *.callback package

--- Android/app/src/main/java/com/rrrfffrrr/unity/kakaotalk/UnityKakaotalkAPI.kt
+++ Android/app/src/main/java/com/rrrfffrrr/unity/kakaotalk/UnityKakaotalkAPI.kt
@@ -14,6 +14,7 @@ import com.kakao.sdk.talk.model.Friend
 import com.kakao.sdk.talk.model.Friends
 import com.kakao.sdk.talk.model.Order
 import com.kakao.sdk.user.UserApiClient
+import com.rrrfffrrr.unity.kakaotalk.callback.*
 
 
 class UnityKakaotalkAPI {
--- Android/app/src/main/java/com/rrrfffrrr/unity/kakaotalk/callback/LoginCallback.kt
+++ Android/app/src/main/java/com/rrrfffrrr/unity/kakaotalk/callback/LoginCallback.kt
@@ -1,4 +1,4 @@
-package com.rrrfffrrr.unity.kakaotalk
+package com.rrrfffrrr.unity.kakaotalk.callback
 
 interface LoginCallback {
     public fun onSuccess(tokenJson: String)
